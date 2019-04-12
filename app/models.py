fe8fca33dd7932fb2111fc0e1d4751e21ce7563c
Aleksi Lehmus <aleksilehmus@Aleksi-MacBook-Air.local>
2019-04-12 22:03:45 +0300

    poistettu turhia

--- app/models.py
+++ app/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
-from app.static.app.sampledata import aikataulut
 
 # Create your models here.
 
